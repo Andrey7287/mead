7d1dacc4aa2e2bd3c57166680bdec93f980384b9
Andrey7287 <banderloginorama@gmail.com>
2017-08-16 01:05:51 +0300

    test

---
+++ public/js/temp.js
@@ -0,0 +1,185 @@
+function PhonePattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    var str = this.$obj.val().replace(/[^0-9]/g,'');
+    return str.length === 11;
+  };
+  this.toString = function() {
+    return "PhonePattern";
+  }
+}
+ 
+ 
+function MobilePhonePattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    var str = this.$obj.val().replace(/[^0-9]/g,'');
+    return  str.length === 11 && isCellPhone(str);
+  };
+  this.toString = function() {
+    return "MobilePhonePattern";
+  }
+}
+ 
+function EmailPattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    var EMAIL_PATTERN = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/i;
+    if (this.$obj.val().replace(/\s+/g,'') !== '') {
+      return (EMAIL_PATTERN.test(this.$obj.val()));
+    } else {
+      return false;
+    }
+  };
+  this.toString = function() {
+    return "EmailPattern";
+  }
+}
+ 
+function TextPattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    return !!this.$obj.value;
+  };
+  this.toString = function() {
+    return "TextPattern";
+  }
+}
+ 
+function PasswordPattern(_min, _max) {
+      var $obj;
+      var min = _min;
+      var max = _max;
+      this.set = function($obj) {
+            this.$obj = $obj;
+      };
+      this.check = function() {
+            if (this.$obj.length < 1) return false;
+            if (this.$obj.val().replace(/\s+/g,'').length < min || this.$obj.val().replace(/\s+/g,'').length > max) return false;
+            var PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/i;
+            if (this.$obj.val().replace(/\s+/g,'') !== '') {
+                  return (PASSWORD_PATTERN.test(this.$obj.val()));
+            } else {
+                  return false;
+            }
+      };
+      this.toString = function() {
+            return "PasswordPattern";
+      }
+}
+ 
+function NumberPattern(length) {
+  var $obj;
+  var req_length = length === undefined ? req_length = -1 : req_length = length;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    var str = this.$obj.val().replace(/[^0-9]/g,'');
+    this.$obj.val(str);
+    if (str.length > 0) {
+      if (req_length === -1) {
+        return true;
+      } else {
+        return  str.length === req_length;
+      }
+    } else {
+      return  false;
+    }
+ 
+  };
+  this.toString = function() {
+    return "NumberPattern ["+req_length+"]";
+  }
+}
+ 
+ 
+function SelectPattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    return this.$obj.val() && this.$obj.val() !== "";
+  };
+  this.toString = function() {
+    return "SelectPattern";
+  }
+}
+ 
+function NonZeroPattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    if (this.$obj.length < 1) return false;
+    return this.$obj.val().replace(/\s+/g,'') !== 0;
+  };
+  this.toString = function() {
+    return "NonZeroPattern";
+  }
+}
+ 
+function RadioPattern() {
+  var $obj;
+  this.set = function($obj) {
+    this.$obj = $obj;
+  };
+  this.check = function() {
+    return this.$obj.length > 0;
+  };
+  this.toString = function() {
+    return "RadioPattern";
+  }
+}
+ 
+ 
+function Validator($obj, pattern, alert_message) {
+  this.$obj = $obj;
+  pattern === undefined ? this.pattern = new TextPattern() : this.pattern = pattern;
+  alert_message === undefined ? this.alert_message = '' : this.alert_message = alert_message;
+ 
+  this.validate = function() {
+    pattern.set(this.$obj);
+    if (pattern.check()) {
+      this.$obj.css('background-color', 'green');
+      return true;
+    } else {
+      this.$obj.css('background-color', 'red');
+      if (!this.$obj[0].value.trim()) {
+        console.log("!")
+        //this.$obj.val("")
+        
+      };
+      if (this.alert_message !== '') alert(this.alert_message);
+      this.$obj.focus();
+      return false;
+    }
+  };
+  this.toString = function() {
+    return "pattern type: " + this.pattern.toString() + ", object: " + this.$obj.length + ", message: " + this.alert_message;
+  };
+      this.val = function() {
+            return this.$obj.val();
+      }
+ 
+}
